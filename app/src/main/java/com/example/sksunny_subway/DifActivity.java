2d99e79628f35b49586d97ab0b9f0f57b160ae65
hummingbbird <pop9814@kookmin.ac.kr>
2023-05-25 19:17:19 +0900

    Add: 컴포넌트 기본 설정 추가(버튼 연결)

---
+++ app/src/main/java/com/example/sksunny_subway/DifActivity.java
@@ -0,0 +1,20 @@
+package com.example.sksunny_subway;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class DifActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dif);
+
+        TextView btn_modify = findViewById(R.id.txtEleven);
+        TextView btn_complete = findViewById(R.id.txtTwelve);
+    }
+}
--- app/src/main/java/com/example/sksunny_subway/MainActivity.java
+++ app/src/main/java/com/example/sksunny_subway/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.sksunny_subway;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.btn);
+        ImageButton btn_search = (ImageButton)findViewById(R.id.imageSearch);
+        EditText Et_Search = (EditText)findViewById(R.id.Et_Search);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
