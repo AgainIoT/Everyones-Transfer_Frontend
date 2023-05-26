8f6445d3f5122b7800606beafad3eb7a1dda4387
hummingbbird <pop9814@kookmin.ac.kr>
2023-05-26 20:49:53 +0900

    Add: 버튼 컴포넌트 선언

--- app/src/main/java/com/example/sksunny_subway/DifActivity.java
+++ app/src/main/java/com/example/sksunny_subway/DifActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class DifActivity extends AppCompatActivity {
 
@@ -16,5 +19,12 @@ public class DifActivity extends AppCompatActivity {
 
         TextView btn_modify = findViewById(R.id.txtEleven);
         TextView btn_complete = findViewById(R.id.txtTwelve);
+
+        EditText btn_elevator = findViewById(R.id.etSix);
+        TextView btn_move = findViewById(R.id.txtOptionOne);
+        EditText btn_turnstile = findViewById(R.id.etOptionTwo);
+        TextView btn_getoff = findViewById(R.id.txtOptionThree);
+
+
     }
 }
--- app/src/main/java/com/example/sksunny_subway/RegisterActivity.java
+++ app/src/main/java/com/example/sksunny_subway/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.sksunny_subway;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        LinearLayout btn_next = findViewById(R.id.linearRoweleven);
+        LinearLayout btn_finish = findViewById(R.id.linearRowtwelve);
+
     }
 }
