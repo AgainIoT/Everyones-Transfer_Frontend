14e5197dbda398b81cd9cddbc8d7350d3de7b960
Kim-Jiyun <84488029+Kim-Jiyun@users.noreply.github.com>
2023-05-27 17:04:43 +0900

    Fix : 검색 버튼 오류 수정

--- app/src/main/java/com/example/sksunny_subway/MainActivity.java
+++ app/src/main/java/com/example/sksunny_subway/MainActivity.java
@@ -7,29 +7,40 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
-//    Spinner departSelector = findViewById(R.id.spinnerTwoTwo);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //백엔드로 받아온 라인 정보
+        ArrayList<String> lines = new ArrayList<>();
+        AdapterSpinner adapterlines;
 
-//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.stations, android.R.layout.simple_spinner_item);
-//        departSelector.setAdapter(adapter);
-
-        Button btn = (Button)findViewById(R.id.btn);
-        ImageButton btn_search = (ImageButton)findViewById(R.id.imageSearch);
+        Button register_btn = (Button)findViewById(R.id.register_btn);
+        ImageView Image_Search = findViewById(R.id.Image_Search);
         EditText Et_Search = (EditText)findViewById(R.id.Et_Search);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        Image_Search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String station = Et_Search.getText().toString();
+            }
+        });
+
+        register_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
