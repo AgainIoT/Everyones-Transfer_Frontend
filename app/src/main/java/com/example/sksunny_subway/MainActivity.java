d02424c4d08634083e571b1ec989cbb2ca6ab15c
Kim-Jiyun <84488029+Kim-Jiyun@users.noreply.github.com>
2023-05-29 22:31:02 +0900

    Feat : 메인 엑티비티 앱이 꺼져도 정보 유지 구현

--- app/src/main/java/com/example/sksunny_subway/MainActivity.java
+++ app/src/main/java/com/example/sksunny_subway/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.ArrayMap;
 import android.util.Log;
 import android.text.InputType;
@@ -51,7 +52,8 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
     final String API_KEY = BuildConfig.API_KEY;
 
-    ArrayList<String> lines = new ArrayList<>();    // train lines passing through the station
+    ArrayList<String> lines = new ArrayList<>(); // train lines passing through the station
+    private static final String SETTINGS_PLAYER_JSON = "settings_item_json";
     AdapterSpinner adapterlines;
 
     EditText Et_Search;
@@ -60,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
     Spinner startLine_spinner;
     Spinner endLine_spinner;
     static RequestQueue requestQueue;
+    Boolean search_result = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,21 +71,48 @@ public class MainActivity extends AppCompatActivity {
 
         startLine_spinner = findViewById(R.id.spinner_stline);
         endLine_spinner = findViewById(R.id.spinner_arline);
-        adapterlines = new AdapterSpinner(this, lines); //그 값을 넣어줌
-        startLine_spinner.setAdapter(adapterlines);
-        endLine_spinner.setAdapter(adapterlines);
 
         lines.add("호선 입력");
 
         Button register_btn = findViewById(R.id.register_btn);
         ImageView Image_Search = findViewById(R.id.Image_Search);
         Et_Search = findViewById(R.id.Et_Search);
-        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
 
         start_nextst = findViewById(R.id.start_nextst);
         arrive_nextst = findViewById(R.id.arrive_nextst);
-        start_nextst.setInputType(InputType.TYPE_NULL);
-        arrive_nextst.setInputType(InputType.TYPE_NULL);
+
+        try{
+            SharedPreferences shared_et_search = getSharedPreferences("et_search", MODE_PRIVATE);    // test 이름의 기본모드 설정, 만약 test key값이 있다면 해당 값을 불러옴.
+            String str_et_search = shared_et_search.getString("et_search","");
+            Et_Search.setText(str_et_search);
+
+            SharedPreferences shared_start_nextst = getSharedPreferences("start_nextst", MODE_PRIVATE);    // test 이름의 기본모드 설정, 만약 test key값이 있다면 해당 값을 불러옴.
+            String str_start_nextst = shared_start_nextst.getString("start_nextst","");
+            start_nextst.setText(str_start_nextst);
+
+            SharedPreferences shared_arrive_nextst = getSharedPreferences("arrive_nextst", MODE_PRIVATE);    // test 이름의 기본모드 설정, 만약 test key값이 있다면 해당 값을 불러옴.
+            String str_arrive_nextst= shared_arrive_nextst.getString("arrive_nextst","");
+            arrive_nextst.setText(str_arrive_nextst);
+
+            lines= getStringArrayPref(getApplicationContext(), SETTINGS_PLAYER_JSON);
+
+            SharedPreferences shared_search_result = getSharedPreferences("search_result", MODE_PRIVATE);    // test 이름의 기본모드 설정, 만약 test key값이 있다면 해당 값을 불러옴.
+            search_result = shared_search_result.getBoolean("search_result", false);
+        }
+        catch(Exception e){
+
+        }
+
+        if(!search_result){
+            start_nextst.setInputType(InputType.TYPE_NULL);
+            arrive_nextst.setInputType(InputType.TYPE_NULL);
+        }
+
+        adapterlines = new AdapterSpinner(this, lines); //그 값을 넣어줌
+        startLine_spinner.setAdapter(adapterlines);
+        endLine_spinner.setAdapter(adapterlines);
+
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
 
         SharedPreferences cookie = getSharedPreferences("cookie", Context.MODE_PRIVATE);
 
@@ -102,6 +132,65 @@ public class MainActivity extends AppCompatActivity {
             String arrive_next = arrive_nextst.getText().toString();
             getRoot();
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SharedPreferences shared_et_search= getSharedPreferences("et_search", MODE_PRIVATE);    // test 이름의 기본모드 설정
+        SharedPreferences.Editor editor_et_search= shared_et_search.edit(); //sharedPreferences를 제어할 editor를 선언
+        editor_et_search.putString("et_search",Et_Search.getText().toString()); // key,value 형식으로 저장
+        editor_et_search.commit();
+
+        SharedPreferences shared_start_nextst= getSharedPreferences("start_nextst", MODE_PRIVATE);    // test 이름의 기본모드 설정
+        SharedPreferences.Editor editor_start_nextst = shared_start_nextst.edit(); //sharedPreferences를 제어할 editor를 선언
+        editor_start_nextst.putString("start_nextst",start_nextst.getText().toString()); // key,value 형식으로 저장
+        editor_start_nextst.commit();
+
+        SharedPreferences shared_arrive_nextst= getSharedPreferences("arrive_nextst", MODE_PRIVATE);    // test 이름의 기본모드 설정
+        SharedPreferences.Editor editor_arrive_nextst = shared_arrive_nextst.edit(); //sharedPreferences를 제어할 editor를 선언
+        editor_start_nextst.putString("arrive_nextst",arrive_nextst.getText().toString()); // key,value 형식으로 저장
+        editor_arrive_nextst.commit();
+
+        setStringArrayPref(getApplicationContext(), SETTINGS_PLAYER_JSON, lines);
+
+        SharedPreferences shared_search_result= getSharedPreferences("search_result", MODE_PRIVATE);    // test 이름의 기본모드 설정
+        SharedPreferences.Editor editor_search_result = shared_search_result.edit(); //sharedPreferences를 제어할 editor를 선언
+        editor_search_result.putBoolean("search_result",search_result); // key,value 형식으로 저장
+        editor_search_result.commit();
+    }
+
+    private void setStringArrayPref(Context context, String key, ArrayList<String> values) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = prefs.edit();
+        JSONArray a = new JSONArray();
+        for (int i = 0; i < values.size(); i++) {
+            a.put(values.get(i));
+        }
+        if (!values.isEmpty()) {
+            editor.putString(key, a.toString());
+        } else {
+            editor.putString(key, null);
+        }
+        editor.apply();
+    }
+
+    private ArrayList<String> getStringArrayPref(Context context, String key) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        String json = prefs.getString(key, null);
+        ArrayList<String> urls = new ArrayList<String>();
+        if (json != null) {
+            try {
+                JSONArray a = new JSONArray(json);
+                for (int i = 0; i < a.length(); i++) {
+                    String url = a.optString(i);
+                    urls.add(url);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return urls;
     }
 
     public void getStationList() {
@@ -136,7 +225,7 @@ public class MainActivity extends AppCompatActivity {
                                 if (adapterlines != null) {
                                     adapterlines.notifyDataSetChanged();
                                 }
-
+                                search_result = true;
                                 start_nextst.setInputType(InputType.TYPE_TEXT_VARIATION_SHORT_MESSAGE);
                                 arrive_nextst.setInputType(InputType.TYPE_TEXT_VARIATION_SHORT_MESSAGE);
 
@@ -163,6 +252,7 @@ public class MainActivity extends AppCompatActivity {
                 public void onErrorResponse(VolleyError error) {
                     start_nextst.setInputType(InputType.TYPE_NULL);
                     arrive_nextst.setInputType(InputType.TYPE_NULL);
+                    search_result = false;
                     NetworkResponse respone = error.networkResponse;
                     if (error instanceof ServerError && respone != null) {
                         Toast.makeText(getApplicationContext(), "정확한 역 이름을 입력해주세요", Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/sksunny_subway/RecyclerAdapter.java
+++ app/src/main/java/com/example/sksunny_subway/RecyclerAdapter.java
@@ -93,9 +93,6 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
     }
 
 
-
-
-
     // onBindViewHolder() - position에 해당하는 데이터를 뷰홀더의 아이템뷰에 표시.
     @Override
     public void onBindViewHolder(@NonNull RecyclerAdapter.ViewHolder holder, int position) {
