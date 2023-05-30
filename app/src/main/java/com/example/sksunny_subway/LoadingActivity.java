9be1461052b281a220e30b1df0dfac6ef1ea8db3
Kim-Jiyun <84488029+Kim-Jiyun@users.noreply.github.com>
2023-05-30 21:29:28 +0900

    Fix : 로딩화면 오류 수정 및 x 표시 제거

--- app/src/main/java/com/example/sksunny_subway/LoadingActivity.java
+++ app/src/main/java/com/example/sksunny_subway/LoadingActivity.java
@@ -2,6 +2,7 @@ package com.example.sksunny_subway;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -11,15 +12,17 @@ public class LoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
-        startLoading();
+        //로딩화면 시작.
+        Loadingstart();
     }
-    private void startLoading() {
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
+    private void Loadingstart(){
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
                 finish();
             }
-        }, 2000);
+        },2000);
     }
 }
